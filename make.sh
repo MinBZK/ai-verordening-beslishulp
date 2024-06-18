9620479c57dace765e249dd6e87a0e99f9462624
IsabelB <126597286+IsabelBarbera@users.noreply.github.com>
2024-06-19 00:50:02 +0200

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./expand-tree.sh transparantie-tree-compressed.json
+./expand-tree.sh subtree-ex-role-compressed.json
+./expand-tree.sh determine-role-ex-transparantie-inc-subtree-compressed.json
+./expand-tree.sh decision-tree-compressed.json
