63e04ff5d62362f0a738490c85e45b4652d3e5e9
ruthkoole <ruth.koole@rijksoverheid.nl>
2024-06-26 11:29:18 +0200

    wrap the results and cleanup code

--- yaml2mermaid.py
+++ yaml2mermaid.py
@@ -29,7 +29,7 @@ def parse_tree(tree):
             if 'result' in answer:
                 if answer['result'] not in results:
                     results.append(answer['result'])
-                    node_result = Node(f"result.{i}", answer['result'])
+                    node_result = Node(f"result.{i}", "`" + answer['result'] + "`")
                     i += 1
                     nodes.append(node_result)
 
@@ -50,37 +50,7 @@ chart = MermaidDiagram(
     links=edges
 )
 
-# print(nodes)
 print(chart)
 
 with open('graph.md', 'w') as f:
     f.write(chart.string)
-
-# def parse_tree(tree, root, id="0"):
-#     for option in tree.get("options", []):
-#         if "text" in option:
-#             o_node = Node(f"{id}.node", option.get("text"))
-#             o_result = Node(f"{id}.result", option.get("result"))
-#             edges.append(o_node)
-#             edges.append(o_result)
-#             edges.append(Link(root, o_node))
-#             edges.append(Link(o_node, o_result))
-#             if "next" in option:
-#                 for i, t in enumerate(option.get("next")):
-#                     parse_tree(t, o_result, f"{id}.{i}")
-#         else:
-#             o_node = Node(f"{id}.question", option.get("question"))
-#             edges.append(o_node)
-#             edges.append(Link(q_node, o_node))
-#             parse_tree(option, o_node, f"{id}.0")
-
-
-# decision_tree = yaml.load(open("decision-tree.yaml", "r"))
-# q_node = Node("root_question", decision_tree["question"])
-# nodes.append(q_node)
-# parse_tree(decision_tree, q_node)
-
-
-
-# with open('graph.md', 'w') as f:
-#     f.write(chart.string)
