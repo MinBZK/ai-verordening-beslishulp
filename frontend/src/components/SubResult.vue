de6cf270bc5511b2f28de3f3bed0d2c914badb99
Laurens Weijs <laurens.weijs@rijksoverheid.nl>
2024-09-24 14:53:59 +0200

    Remove teaser at resultaten

--- frontend/src/components/SubResult.vue
+++ frontend/src/components/SubResult.vue
@@ -91,7 +91,6 @@ function get_background_color(label: string | undefined) {
           ></span>
           {{ title }}
         </h3>
-              <span class="rvo-accordion-teaser">Bekijk hier je profiel tot nu toe</span>
       </summary>
       <div class="rvo-accordion__content">
         <div class="rvo-table--responsive py-5">
