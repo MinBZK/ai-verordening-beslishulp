7af7d1a468c4501b29ce917bf7228e1a89d4ff35
Laurens Weijs <laurens.weijs@rijksoverheid.nl>
2024-09-25 08:33:55 +0200

    Make betaversion expandable

--- frontend/src/components/betaversion-label.vue
+++ frontend/src/components/betaversion-label.vue
@@ -8,7 +8,19 @@
               aria-label="Waarschuwing"
             ></span>
       <div class="utrecht-alert--warning utrecht-paragraph">
-        Bètaversie
+        <details class="rvo-expandable-text rvo-expandable-text--subtle">
+          <summary class="rvo-expandable-text__summary">
+            Bètaversie
+          </summary>
+          <div class="rvo-expandable-text__details absolute"
+               style="margin: 20px;padding: 10px; border-radius: 5px; background-color: var(--rvo-alert-warning-background-color); width: 150px; z-index: 2">
+            Deze website is in ontwikkeling. Alle versies onstaan op een open manier.
+            <a class="rvo-link" href="https://github.com/MinBZK/ai-act-decisiontree">
+              Iedereen
+              mag opmerkingen en suggesties doen.
+            </a>
+          </div>
+        </details>
       </div>
     </div>
   </div>
