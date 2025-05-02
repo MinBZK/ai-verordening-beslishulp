cb4fb03c3e67404d878183b5759b789694d4e6b6
Ravi Meijer <ravi.meijer@rijksoverheid.nl>
2025-05-02 16:55:43 +0200

    Add examples algorithm audit to beslishulp and add guidance on 'toelichtingen' veld (#431)
    Co-authored-by: Robbert Bos <robbert.bos@rijksoverheid.nl>

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -5,81 +5,70 @@ const emit = defineEmits(['acceptDisclaimer'])
 </script>
 
 <template>
-  <Header :questionId="'1.1'" :disclaimer-screen="'0'" />
-  <main>
-    <div class="rvo-progress-tracker"></div>
-    <div class="rvo-form">
-      <div class="flex items-center rvo-layout-column rvo-layout-gap--xl">
-        <div class="flex justify-center rvo-form-intro inline-flex py-5">
-          <h2 class="flex justify-center utrecht-heading-2">Beslishulp AI-verordening</h2>
-        </div>
-        <div
-          class="rvo-layout-column rvo-layout-gap--md rvo-max-width-layout rvo-max-width-layout--lg"
-        >
-          <div class="utrecht-form-fieldset rvo-form-fieldset">
-            <fieldset class="utrecht-form-fieldset__fieldset utrecht-form-fieldset--html-fieldset">
-              <p class="utrecht-paragraph">
-                Met deze beslishulp kun je nalopen of de AI-verordening geldt voor een toepassing
-                die jouw overheidsorganisatie gebruikt. En aan welke vereisten je dan moet voldoen
-                voor deze toepassing. Let op: je moet dus voor elke toepassing apart nalopen of de
-                AI-verordening geldt en wat dat betekent. Verder zijn de volgende zaken van belang:
-              </p>
-              <ul class="rvo-ul rvo-ul--no-margin rvo-ul--no-padding utrecht-unordered-list">
-                <li>
-                  Deze beslishulp is een betaversie: de informatie is niet compleet en er kunnen
-                  fouten in staan. Kom je iets tegen? Mail naar:
-                  <a class="rvo-link" href="mailto:ai-verordening@minbzk.nl">
-                    ai-verordening@minbzk.nl</a
-                  >.
-                </li>
-                <li>
-                  Je doorloopt de beslishulp op basis van je eigen antwoorden. De beslishulp kan
-                  niet vaststellen of jouw antwoorden waar zijn of niet.
-                </li>
-                <li>
-                  De beslishulp is geen check op jouw eigen kennis en inzichten en je blijft zelf
-                  verantwoordelijk voor de informatie die je gebruikt.
-                </li>
-                <li>
-                  Wij adviseren de uitkomsten van de beslishulp met andere experts in jouw
-                  organisatie te delen en te bespreken.
-                </li>
-                <li>
-                  De beslishulp is bedoeld voor overheden die AI-systemen aanbieden, of gebruiken
-                  onder eigen verantwoordelijkheid.
-                </li>
-                <li>
-                  Voor vragen of opmerkingen over de beslishulp mail je naar:
-                  <a class="rvo-link" href="mailto:ai-verordening@minbzk.nl">
-                    ai-verordening@minbzk.nl</a
-                  >.
-                </li>
-              </ul>
-              <p class="utrecht-paragraph">
-                Om je te helpen bij het beantwoorden van de vragen, zijn relevante overwegingen /
-                artikelen / bijlagen van de AI-verordening gelinkt per vraag. Definities van termen
-                worden laten zien als je met je muis eroverheen gaat en zijn terug te vinden op het
-                <a
-                  class="rvo-link"
-                  href="https://minbzk.github.io/Algoritmekader/overhetalgoritmekader/definities/?h=definitie"
-                  target="_blank"
-                >
-                  Algoritmekader.
-                </a>
-              </p>
-            </fieldset>
+    <Header
+      :questionId="'1.1'"
+      :disclaimer-screen="'0'"/>
+    <main>
+      <div class="rvo-progress-tracker">
+      </div>
+      <div class="rvo-form">
+        <div class="flex items-center rvo-layout-column rvo-layout-gap--xl">
+          <div class="flex justify-center rvo-form-intro inline-flex py-5">
+            <h2 class="flex justify-center utrecht-heading-2">Beslishulp AI-verordening</h2>
+          </div>
+          <div class="rvo-layout-column rvo-layout-gap--md rvo-max-width-layout rvo-max-width-layout--lg">
+            <div class="utrecht-form-fieldset rvo-form-fieldset">
+              <fieldset
+                class="utrecht-form-fieldset__fieldset utrecht-form-fieldset--html-fieldset"
+              >
+                <p class="utrecht-paragraph">Met deze beslishulp kun je nalopen of de AI-verordening geldt
+                  voor een toepassing die jouw overheidsorganisatie gebruikt. En aan welke vereisten je dan
+                  moet voldoen voor deze toepassing. Aan het einde van de beslishulp kan je de resultaten
+                  exporteren in een rapport. Eventuele toelichtingen om context vast te leggen bij een keuze,
+                  worden in dit rapport opgenomen.</p>
+                 <p class="utrecht-paragraph">Let op: je moet dus voor elke toepassing apart nalopen of de
+                 AI-verordening geldt en wat dat betekent. Verder zijn de volgende zaken van belang:</p>
+                <ul class="rvo-ul rvo-ul--no-margin rvo-ul--no-padding utrecht-unordered-list">
+                  <li>Deze beslishulp is een betaversie: de informatie is niet compleet en er kunnen fouten in staan.
+                    Kom je iets tegen? Mail naar:
+                    <a
+                      class="rvo-link" href="mailto:ai-verordening@minbzk.nl">
+                      ai-verordening@minbzk.nl</a>.
+                  </li>
+                  <li>Je doorloopt de beslishulp op basis van je eigen antwoorden. De beslishulp kan niet vaststellen of jouw antwoorden waar zijn of niet.
+                  </li>
+                  <li>De beslishulp is geen check op jouw eigen kennis en inzichten en je blijft zelf verantwoordelijk voor de informatie die je gebruikt.</li>
+                  <li>Wij adviseren de uitkomsten van de beslishulp met andere experts in jouw organisatie te delen en te bespreken.</li>
+                  <li>De beslishulp is bedoeld voor overheden die AI-systemen
+                    aanbieden, of gebruiken onder eigen
+                    verantwoordelijkheid.
+                  </li>
+                  <li> Voor vragen of opmerkingen over de beslishulp mail je naar:
+                    <a
+                      class="rvo-link" href="mailto:ai-verordening@minbzk.nl">
+                      ai-verordening@minbzk.nl</a>.
+                  </li>
+                  <li>
+                    Om je te helpen bij het beantwoorden van de vragen, zijn relevante
+                    overwegingen / artikelen / bijlagen
+                    van de AI-verordening gelinkt per vraag. Definities van termen worden getoond als je met je muis
+                    eroverheen gaat en zijn terug te vinden op het
+                    <a class="rvo-link" href="https://minbzk.github.io/Algoritmekader/overhetalgoritmekader/definities/?h=definitie" target="_blank">
+                      Algoritmekader.
+                    </a>
+                  </li>
+                </ul>
+                <br>
+              </fieldset>
+            </div>
+          </div>
+          <div class="py-5 px-5">
+            <button @click="$emit('acceptDisclaimer')" type="button"
+                    class="utrecht-button utrecht-button--primary-action utrecht-button--rvo-md rvo-link--no-underline">
+              Akkoord en begin met de beslishulp
+            </button>
           </div>
         </div>
-        <div class="py-5 px-5">
-          <button
-            @click="$emit('acceptDisclaimer')"
-            type="button"
-            class="utrecht-button utrecht-button--primary-action utrecht-button--rvo-md rvo-link--no-underline"
-          >
-            Akkoord en begin met de beslishulp
-          </button>
-        </div>
       </div>
-    </div>
   </main>
 </template>
